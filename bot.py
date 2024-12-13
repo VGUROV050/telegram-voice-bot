3fe262ebde04202f9a726b1670a9739c44c2ac24
Vladislav Gurov <vgurov@Vladislavs-MacBook-Pro-14.local>
2024-12-13 12:29:34 +0400

    Fix download method for telegram File object

--- bot.py
+++ bot.py
@@ -1,19 +1,27 @@
 from telegram import Update
-from telegram.ext import Application, MessageHandler, CallbackContext, filters
+from telegram.ext import Application, CommandHandler, MessageHandler, CallbackContext, filters
 import os
 from pydub import AudioSegment
 import speech_recognition as sr
 
 # Конфигурация
 BOT_TOKEN = os.getenv('BOT_TOKEN')  # Токен передадим через Render
+# ID чата пользователя, которому нужно отправить текст
+GROUP_CHAT_ID = -1002433054865  # Замените на реальный chat_id
+
+async def get_chat_id(update: Update, context: CallbackContext) -> None:
+    chat_id = update.message.chat_id
+    print(f"Chat ID: {chat_id}")  # Выводит chat_id группы в консоль
+    await update.message.reply_text(f"Group Chat ID: {chat_id}")
 
 # Функция для обработки голосовых сообщений
 async def handle_voice(update: Update, context: CallbackContext) -> None:
+    # Получение файла голосового сообщения
     voice_file = await update.message.voice.get_file()
     file_path = "voice.ogg"
 
     # Скачиваем аудио
-    await voice_file.download(file_path)
+    await voice_file.download_to_drive(file_path)
 
     # Конвертируем в WAV
     audio = AudioSegment.from_file(file_path)
@@ -26,11 +34,12 @@ async def handle_voice(update: Update, context: CallbackContext) -> None:
         audio_data = recognizer.record(source)
         try:
             text = recognizer.recognize_google(audio_data, language="ru-RU")
-            await update.message.reply_text(f"Распознанный текст: {text}")
+            # Отправляем текст в группу
+            await context.bot.send_message(chat_id=GROUP_CHAT_ID, text=text)
         except sr.UnknownValueError:
-            await update.message.reply_text("Не удалось распознать текст.")
+            await context.bot.send_message(chat_id=GROUP_CHAT_ID, text="Не удалось распознать текст.")
         except sr.RequestError as e:
-            await update.message.reply_text(f"Ошибка распознавания: {e}")
+            await context.bot.send_message(chat_id=GROUP_CHAT_ID, text=f"Ошибка распознавания: {e}")
 
     # Удаляем временные файлы
     os.remove(file_path)
@@ -38,7 +47,8 @@ async def handle_voice(update: Update, context: CallbackContext) -> None:
 
 # Запуск бота
 def main() -> None:
-    app = Application.builder().token(BOT_TOKEN).build()
+    BOT_TOKEN = os.getenv("BOT_TOKEN")
+    app = Application.builder().token(BOT_TOKEN).connect_timeout(30).read_timeout(30).build()
 
     # Обработчики
     app.add_handler(MessageHandler(filters.VOICE, handle_voice))
